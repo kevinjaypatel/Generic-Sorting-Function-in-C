deaae6692514e7cd1e737e55a362008348accbe1
Kevin Patel <kevinjaypatel@gmail.com>
2020-10-10 11:34:52 -0700

    designed an array of person structs known as people, created an array of floating digits, defined constants for the total number of people structs and floating digits

--- genericProgram.c
+++ genericProgram.c
@@ -3,33 +3,49 @@
 // standard library header file 
 #include <stdlib.h>
 
-struct person {
+// constant number of people for reference 
+#define NUM_PEOPLE 17
+// constant number of floating digits for reference 
+#define TOTAL_NUMBERS 12 
+
+typedef struct {
     int age; 
-    // char* name; 
-}; 
+    char name[15]; 
+} Person; 
 
 int main(void) {
 
     // initialize an array of persons (aka people)
-    struct person people[17] = {
-        [0] = { 20 }, 
-        [1] = { 31 }, 
-        [2] = { 19 }, 
-        [3] = { 21 }, 
-        [4] = { 25 },
-        [5] = { 22 }, 
-        [6] = { 27 }, 
-        [7] = { 19 }, 
-        [8] = { 27 }, 
-        [9] = { 32 }, 
-        [10] = { 20 }, 
-        [11] = { 23 }, 
-        [12] = { 26 }, 
-        [13] = { 24 }, 
-        [14] = { 28 }, 
-        [15] = { 33 }, 
-        [16] = { 25 }
+    Person people[] = {
+        { 20, "Hal" }, 
+        { 31, "Susann" }, 
+        { 19, "Dwight" }, 
+        { 21, "Kassandra" }, 
+        { 25, "Lawrence" },
+        { 22, "Cindy" }, 
+        { 27, "Cory" }, 
+        { 19, "Mac" }, 
+        { 27, "Romana" }, 
+        { 32, "Doretha" }, 
+        { 20, "Danna" }, 
+        { 23, "Zara" }, 
+        { 26, "Rosalyn" }, 
+        { 24, "Risa" }, 
+        { 28, "Benny" }, 
+        { 33, "Juan" }, 
+        { 25, "Natalie" }
     }; 
+
+    // initialize an array of floating point numbers
+    float numbers[] = {645.32, 37.40, 76.30, 5.40, -34.23, 1.11, -34.94, 23.37, 635.46, -876.22, 467.73, 62.26}; 
+
+    for(int i = 0; i < NUM_PEOPLE; i++){
+        printf( "Name: %s, Age: %d\n", people[i].name, people[i].age); 
+    }
+
+    for(int i = 0; i < TOTAL_NUMBERS; i++){
+        printf( "Number: %f\n", numbers[i]); 
+    }
  
     // terminate program 
     return 0; 
