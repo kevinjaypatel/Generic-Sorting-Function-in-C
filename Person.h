ba45495d44a536d1c36f479f13e6f831bfd85e7b
Kevin Patel <kevinjaypatel@gmail.com>
2020-10-11 23:07:44 -0700

    added a person header file for storing the Person struct

--- Person.h
+++ Person.h
@@ -7,4 +7,7 @@ typedef struct {
     char name[15]; 
 } Person; 
 
+int compareNames (const void* a, const void* b); 
+int compareAge(const void*a, const void* b); 
+
 #endif /* end of include guard: PERSON_H */
--- genericProgram.c
+++ genericProgram.c
@@ -4,17 +4,13 @@
 #include <stdlib.h>
 // standard string header file
 #include <string.h>
+// person header file
+#include "Person.h"
 
 // constant number of people for reference 
 #define NUM_PEOPLE 17
 // constant number of floating digits for reference 
 #define TOTAL_NUMBERS 12 
-
-typedef struct {
-    int age; 
-    char name[15]; 
-} Person; 
-
 
 // initialize an array of floating point numbers
 float numbers[] = {645.32, 37.40, 76.30, 5.40, -34.23, 1.11, -34.94, 23.37, 635.46, -876.22, 467.73, 62.26}; 
@@ -43,74 +39,53 @@ void SortFloatingNumbers();
 void SortByName(); 
 void SortByAge(); 
 
-void PrintFloatingArray(); 
-void PrintPeoplesArray(); 
-
 int compareFloat (const void * a, const void * b); 
 int compareNames (const void* a, const void* b); 
 int compareAge(const void*a, const void* b); 
 
+void PrintFloatingArray(); 
+void PrintPeoplesArray(); 
+
 int main(void) {
 
-    // SortFloatingNumbers(); 
-    // SortByName(); 
+    SortFloatingNumbers(); 
+    PrintFloatingArray(); 
+
+    SortByName(); 
+    printf("Sorted people by name: \n"); 
+    PrintPeoplesArray(); 
+
     SortByAge(); 
-    
+    printf("Sorted people by age: \n"); 
+    PrintPeoplesArray(); 
+
     // terminate program 
     return 0; 
 }
 
 void SortByAge() {
+    qsort(people, NUM_PEOPLE, sizeof(Person), compareAge);  
+}
 
-    printf("Before sorting the list: \n"); 
-    PrintPeoplesArray(); 
-
-    qsort(people, NUM_PEOPLE, sizeof(Person), compareAge); 
-
-    printf("\nAfter sorting the peoples list is: \n"); 
-    PrintPeoplesArray(); 
+void SortByName() {
+    qsort(people, NUM_PEOPLE, sizeof(Person), compareNames);    
 }
 
 void PrintFloatingArray() {
-    for(int i = 0; i < TOTAL_NUMBERS; i++){
-        printf( "Number: %f\n", numbers[i]); 
+    printf("Sorted numbers in ascending order: \n"); 
+    for(int i = 0; i < TOTAL_NUMBERS; i++) {
+        printf("%f \n", numbers[i]); 
     }
 }
 
 void PrintPeoplesArray() {
     for(int i = 0; i < NUM_PEOPLE; i++) {
-        printf("Name: %s, Age: %d\n", people[i].name, people[i].age); 
+        printf("%s, %i \n", people[i].name, people[i].age); 
     }
 }
 
-void SortFloatingNumbers() {
-
-    printf("Before sorting the list: \n");
-    PrintFloatingArray(); 
-
+void SortFloatingNumbers() { 
     qsort(numbers, TOTAL_NUMBERS, sizeof(float), compareFloat); 
-
-    printf("\nAfter sorting the float list is: \n"); 
-    PrintFloatingArray(); 
-}
-
-void SortByName() {
-
-    printf("Before sorting the names: \n"); 
-    PrintPeoplesArray(); 
-
-    qsort(people, NUM_PEOPLE, sizeof(Person), compareNames); 
-    
-    printf("\nAfter sorting the names: \n"); 
-    PrintPeoplesArray(); 
-}
-
-int compareNames (const void* a, const void* b) {
-
-    Person *firstPerson = (Person*)a; 
-    Person *secondPerson = (Person*)b;
-
-    return strcmp(firstPerson->name, secondPerson->name);  
 }
 
 int compareFloat(const void* a, const void* b) {
@@ -125,7 +100,17 @@ int compareAge(const void* a, const void* b) {
     if(firstPerson->age == secondPerson->age) {
         return compareNames(firstPerson, secondPerson); 
     } 
-    return firstPerson->age - secondPerson->age; 
+    return secondPerson->age - firstPerson->age; 
 }
+
+int compareNames (const void* a, const void* b) {
+
+    Person *firstPerson = (Person*)a; 
+    Person *secondPerson = (Person*)b;
+
+    return strcmp(firstPerson->name, secondPerson->name);  
+}
+
+
     
 
